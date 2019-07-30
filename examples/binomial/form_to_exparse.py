b6f5111808fc292cef6bf408ed443a4be28bf42d
Stephen Jones <s.jones@cern.ch>
2019-07-30 17:19:22 +0200

    [examples] form_to_exparse replaced by symlinks

--- examples/binomial/form_to_exparse.py
+++ examples/binomial/form_to_exparse.py
@@ -1,11 +1,1 @@
-mystr = ''
-
-# Read
-with open('expr.out', 'r') as f:
-  lines = f.readlines()
-  mystr = ''.join([line.strip() for line in lines]).replace(' ','')
-
-# Write
-with open('expr.txt', 'w') as f:
-  f.write(mystr)
-
+../form_to_exparse.py
